52a6370c10e7287f242dad36267e7a07a52e8680
Egeyar <egeyar.bagcioglu@oracle.com>
2019-05-22 17:16:52 +0000

    split-debuginfo.sh

---
+++ split-debuginfo.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+BUILDDIR="/home/egeyar/ol7repos/gcc/el7-u6/rpmbuild/BUILD/gcc-4.8.5-20150702"
+if [ -f "${BUILDDIR}"/debugfiles.list \
+     -a -f "${BUILDDIR}"/debuglinks.list ]; then
+  > "${BUILDDIR}"/debugsources-base.list
+  > "${BUILDDIR}"/debugfiles-base.list
+  cd "${RPM_BUILD_ROOT}"
+  for f in `find usr/lib/debug -name \*.debug \
+	    | egrep 'lib[0-9]*/lib(gcc|gomp|stdc|quadmath|itm)'`; do
+    echo "/$f" >> "${BUILDDIR}"/debugfiles-base.list
+    if [ -f "$f" -a ! -L "$f" ]; then
+      cp -a "$f" "${BUILDDIR}"/test.debug
+      /usr/lib/rpm/debugedit -b "${RPM_BUILD_DIR}" -d /usr/src/debug \
+			     -l "${BUILDDIR}"/debugsources-base.list \
+			     "${BUILDDIR}"/test.debug
+      rm -f "${BUILDDIR}"/test.debug
+    fi
+  done
+  for f in `find usr/lib/debug/.build-id -type l`; do
+    ls -l "$f" | egrep -q -- '->.*lib[0-9]*/lib(gcc|gomp|stdc|quadmath|itm)' \
+      && echo "/$f" >> "${BUILDDIR}"/debugfiles-base.list
+  done
+  grep -v -f "${BUILDDIR}"/debugfiles-base.list \
+    "${BUILDDIR}"/debugfiles.list > "${BUILDDIR}"/debugfiles.list.new
+  mv -f "${BUILDDIR}"/debugfiles.list.new "${BUILDDIR}"/debugfiles.list
+  for f in `LC_ALL=C sort -z -u "${BUILDDIR}"/debugsources-base.list \
+	    | grep -E -v -z '(<internal>|<built-in>)$' \
+	    | xargs --no-run-if-empty -n 1 -0 echo \
+	    | sed 's,^,usr/src/debug/,'`; do
+    if [ -f "$f" ]; then
+      echo "/$f" >> "${BUILDDIR}"/debugfiles-base.list
+      echo "%exclude /$f" >> "${BUILDDIR}"/debugfiles.list
+    fi
+  done
+  mv -f "${BUILDDIR}"/debugfiles-base.list{,.old}
+  echo "%dir /usr/lib/debug" > "${BUILDDIR}"/debugfiles-base.list
+  awk 'BEGIN{FS="/"}(NF>4&&$NF){d="%dir /"$2"/"$3"/"$4;for(i=5;i<NF;i++){d=d"/"$i;if(!v[d]){v[d]=1;print d}}}' \
+    "${BUILDDIR}"/debugfiles-base.list.old >> "${BUILDDIR}"/debugfiles-base.list
+  cat "${BUILDDIR}"/debugfiles-base.list.old >> "${BUILDDIR}"/debugfiles-base.list
+  rm -f "${BUILDDIR}"/debugfiles-base.list.old
+fi
