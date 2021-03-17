3bd17774e9d30fe7ca5f10804ee2f4ef8e908278
Jose E. Marchesi <jose.marchesi@oracle.com>
2021-03-17 12:55:23 +0100

    dwarf: add a dwarf2int.h internal interface
    This patch introduces a dwarf2int.h header, to be used by code that
    needs access to the internal DIE structures and their attributes.
    
    The following functions which were previously defined as static in
    dwarf2out.c are now non-static, and extern prototypes for them have
    been added to dwarf2int.h:
    
    - get_AT
    - AT_int
    - get_AT_ref
    - get_AT_string
    - get_AT_class
    - AT_unsigned
    - get_AT_unsigned
    - get_AT_flag
    - add_name_attribute
    - new_die_raw
    - base_type_die
    - lookup_decl_die
    - get_AT_file
    
    Note how this patch doens't change the names of these functions to
    avoid a massive renaming in dwarf2out.c, but n the future we probably
    want these functions to sport a dw_* prefix.
    
    Also, some type definitions have been moved from dwarf2out.c to
    dwarf2int.h:
    
    - dw_attr_node
    - struct dwarf_file_data
    
    Finally, three new accessor functions have been added to dwarf2out.c
    with prototypes in dwarf2int.h:
    
    - dw_get_die_child
    - dw_get_die_sib
    - dw_get_die_tag
    
    2021-04-14  Jose E. Marchesi  <jose.marchesi@oracle.com>
    
    	* dwarf2int.h: New file.
    	* dwarf2out.c (get_AT): Function is no longer static.
    	(get_AT_string): Likewise.
    	(get_AT_flag): Likewise.
    	(get_AT_unsigned): Likewise.
    	(get_AT_ref): Likewise.
    	(new_die_raw): Likewise.
    	(lookup_decl_die): Likewise.
    	(base_type_die): Likewise.
    	(add_name_attribute): Likewise.
    	(dw_get_die_tag): New function.
    	(dw_get_die_child): Likewise.
    	(dw_get_die_sib): Likewise.
    	Include dwarf2int.h.
    	* gengtype.c: add dwarf2int.h to open_base_files.
    	* Makefile.in (GTFILES): Add dwarf2int.h.

---
+++ gcc/dwarf2int.h
@@ -0,0 +1,67 @@
+/* Prototypes for functions manipulating DWARF2 DIEs.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/* This file contains prototypes for functions defined in dwarf2out.c.  It is
+   intended to be included in source files that need some internal knowledge of
+   the GCC dwarf structures.  */
+
+#ifndef GCC_DWARF2INT_H
+#define GCC_DWARF2INT_H 1
+
+/* Each DIE attribute has a field specifying the attribute kind,
+   a link to the next attribute in the chain, and an attribute value.
+   Attributes are typically linked below the DIE they modify.  */
+
+typedef struct GTY(()) dw_attr_struct {
+  enum dwarf_attribute dw_attr;
+  dw_val_node dw_attr_val;
+}
+dw_attr_node;
+
+extern dw_attr_node *get_AT (dw_die_ref, enum dwarf_attribute);
+extern HOST_WIDE_INT AT_int (dw_attr_node *);
+extern unsigned HOST_WIDE_INT AT_unsigned (dw_attr_node *a);
+extern dw_die_ref get_AT_ref (dw_die_ref, enum dwarf_attribute);
+extern const char *get_AT_string (dw_die_ref, enum dwarf_attribute);
+extern enum dw_val_class AT_class (dw_attr_node *);
+extern unsigned HOST_WIDE_INT AT_unsigned (dw_attr_node *);
+extern unsigned get_AT_unsigned (dw_die_ref, enum dwarf_attribute);
+extern int get_AT_flag (dw_die_ref, enum dwarf_attribute);
+
+extern void add_name_attribute (dw_die_ref, const char *);
+
+extern dw_die_ref new_die_raw (enum dwarf_tag);
+extern dw_die_ref base_type_die (tree, bool);
+
+extern dw_die_ref lookup_decl_die (tree);
+
+extern dw_die_ref dw_get_die_child (dw_die_ref);
+extern dw_die_ref dw_get_die_sib (dw_die_ref);
+extern enum dwarf_tag dw_get_die_tag (dw_die_ref);
+
+/* Data about a single source file.  */
+struct GTY((for_user)) dwarf_file_data {
+  const char * filename;
+  int emitted_number;
+};
+
+extern struct dwarf_file_data *get_AT_file (dw_die_ref,
+					    enum dwarf_attribute);
+
+#endif /* !GCC_DWARF2INT_H */
