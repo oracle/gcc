4d45ed4daa22f835045db7fa8fe7a8d25b3891bd
Qing Zhao <qing.zhao@oracle.com>
2023-07-26 20:27:20 +0000

    gcc12-autoinit-24-104550-suppress-warn-for-new-uses.patch

--- gcc/gimple-fold.c
+++ gcc/gimple-fold.c
@@ -4232,7 +4232,12 @@ clear_padding_flush (clear_padding_struct *buf, bool full)
 	      else
 		{
 		  src = make_ssa_name (type);
-		  g = gimple_build_assign (src, unshare_expr (dst));
+		  tree tmp_dst = unshare_expr (dst);
+		  /* The folding introduces a read from the tmp_dst, we should
+		     prevent uninitialized warning analysis from issuing warning
+		     for such fake read.  */
+		  TREE_NO_WARNING (tmp_dst) = 1; 
+		  g = gimple_build_assign (src, tmp_dst);
 		  gimple_set_location (g, buf->loc);
 		  gsi_insert_before (buf->gsi, g, GSI_SAME_STMT);
 		  tree mask = native_interpret_expr (type,
